fad8aaad1ad95ad368d4deb707f0e665f2c3194c
Samuel Davis <samueljakdavis@gmail.com>
2025-12-27 07:47:17 -0500

    figure out rope

--- cat.gd
+++ cat.gd
@@ -1,30 +1,49 @@
 class_name Cat
 extends CharacterBody2D
 
-@export var speed: float = 200.0
 @export var target: Node2D
+
+@export var speed: float = 100.0
+@export var jump_velocity: float = -400.0
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-var lag: float = 0.0
-
-var target_offset: Vector2:
+var direction: Vector2:
 	get:
 		return target.global_position - global_position
 
 
 func _process(_delta: float) -> void:
-	velocity = target_offset.sign() * speed
+	_face_target()
 
-	velocity.y = 0
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+
+	if direction.abs().x > 5:
+		sprite.play("running")
+	else:
+		sprite.play("idle")
+
+	if not is_on_floor():
+		sprite.play("dance")
 
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if direction.abs().x > 5:
+		velocity.x = direction.normalized().x * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
 	move_and_slide()
 
 
 func _face_target() -> void:
-	if target_offset.x > 0:
+	if direction.x > 0:
 		sprite.flip_h = false
-	if target_offset.x < 0:
+	if direction.x < 0:
 		sprite.flip_h = true
--- rope_anchor.gd
+++ rope_anchor.gd
@@ -4,23 +4,51 @@ extends AnimatableBody2D
 @export var rope_segments: int = 6
 @export var rope_segment_scene: PackedScene
 @export var end: RigidBody2D
+@export var speed: float = 450.0
+@export var dead_zone: float = 5.0
 
 @onready var button: Button = $Button
+@onready var line: Line2D = $Line2D
 
-var mouse_offset: Vector2 = Vector2.ZERO
+var offset: Vector2 = Vector2.ZERO
 
 var attach_point: Node2D:
 	get:
 		return self
 
-var target_position: Vector2:
+var direction: Vector2:
 	get:
-		return get_global_mouse_position() + mouse_offset
+		return get_global_mouse_position() - global_position
 
 
 func _ready() -> void:
 	button.button_down.connect(_on_pressed)
+	_build_rope()
 
+
+func _physics_process(delta: float) -> void:
+	line.global_position = global_position
+	line.points = _get_points()
+
+	if not button.is_pressed():
+		return
+
+	var step = Vector2.ZERO
+	if direction.length() < dead_zone:
+		step = lerp(direction, Vector2.ZERO, 0.5)
+	else:
+		step = direction.normalized() * speed * delta
+
+	var collision: KinematicCollision2D = move_and_collide(step)
+	if collision:
+		print(collision.get_angle())
+
+
+func _on_pressed() -> void:
+	offset = get_global_mouse_position() - global_position
+
+
+func _build_rope() -> void:
 	var previous: Node2D = self
 	for i in rope_segments:
 		var segment: RopeSegment = rope_segment_scene.instantiate()
@@ -36,10 +64,10 @@ func _ready() -> void:
 			break
 
 
-func _physics_process(_delta: float) -> void:
-	if button.is_pressed():
-		global_position = target_position
-
-
-func _on_pressed() -> void:
-	mouse_offset = global_position - get_global_mouse_position()
+func _get_points() -> PackedVector2Array:
+	var points: PackedVector2Array = []
+	for node in get_children():
+		if node is RopeSegment:
+			points.append(node.position)
+	points.append(end.position)
+	return points
