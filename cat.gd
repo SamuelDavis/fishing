5db4e1004fca25f374f3218b120dd88d0a4f8094
Samuel Davis <samueljakdavis@gmail.com>
2025-12-28 11:47:43 -0500

    add cat back

--- cat.gd
+++ cat.gd
@@ -1,56 +1,11 @@
 class_name Cat
-extends RigidBody2D
-
-@export var target: Node2D
-
-@export var speed: float = 100.0
-@export var jump_velocity: float = -400.0
-@export var dead_zone: float = 5.0
+extends CharacterBody2D
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collider: CollisionShape2D = $CollisionShape2D
 
-var caught: bool = false
-var velocity: Vector2 = Vector2.ZERO
-
-var direction: Vector2:
-	get:
-		return target.global_position - global_position
-
-
-func _ready() -> void:
-	body_entered.connect(func(_body): print("enter"))
-	body_exited.connect(func(_body): print("exit"))
-
-
-func is_on_floor() -> bool:
-	return true
-
-
-func move_and_slide() -> void:
-	pass
-
-
-func _process(_delta: float) -> void:
-	if caught:
-		sprite.play("tickle")
-		return
-
-	_face_target()
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_velocity
-
-	if direction.abs().x > dead_zone:
-		sprite.play("running")
-	else:
-		if is_on_floor():
-			velocity.y = jump_velocity
-		sprite.play("idle")
-
-	if not is_on_floor():
-		sprite.play("dance")
+const speed = 300.0
+const jump_velocity = -400.0
 
 
 func _physics_process(delta: float) -> void:
@@ -58,16 +13,16 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if direction.abs().x > 5:
-		velocity.x = direction.normalized().x * speed
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
-
-
-func _face_target() -> void:
-	if direction.x > 0:
-		sprite.flip_h = false
-	if direction.x < 0:
-		sprite.flip_h = true
